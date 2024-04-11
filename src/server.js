aa55b7e6145c80fb93c71700ff370f4fcf3bed4e
youngseojang <youngseojang@gmail.com>
2024-04-11 15:19:34 +0900

    메시지 전송 버튼 생성

--- src/server.js
+++ src/server.js
@@ -11,22 +11,25 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
+const handleListen = () => console.log(`Listening on http://localhost:3000`)
+
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
 function onSocketClose() {
     console.log("Disconnected from the Browser ❌");
-  }
-  
-  function onSocketMessage(message) {
-    console.log(message);
-  }
+}
+
+const sockets = [];
 
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Browser✅");
     socket.on("close", onSocketClose);
-    socket.on("message", onSocketMessage);
-    socket.send("hello!!!!");
+    socket.on("message", (message) => {
+        sockets.forEach((aSocket) =>
+        aSocket.send(message));
+    });
 });
 
 server.listen(3000, handleListen);
